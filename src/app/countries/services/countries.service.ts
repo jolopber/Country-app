ab3f5009def36408c573b906ee04b56715cd4a14
Jose Lopez <jose.lopezberro@gmail.com>
2024-12-26 12:22:37 +0100

    persistencia en cache en las distintas busquedas

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
@@ -53,15 +54,21 @@ export class CountriesService {
 
     }
 
-    searchCountry( country: string ): Observable<Country[]> {
-        const url = `${ this.apiUrl }/name/${ country }`
-        return this.getCountriesRequest(url);
+    searchCountry( term: string ): Observable<Country[]> {
+        const url = `${ this.apiUrl }/name/${ term }`
+        return this.getCountriesRequest(url)
+            .pipe(
+                tap( countries => this.cacheStore.byCountries = { term, countries })
+            )
 
     }
 
-    searchRegion( region: string ): Observable<Country[]> {
+    searchRegion( region: Region ): Observable<Country[]> {
         const url = `${ this.apiUrl }/region/${ region }`
-        return this.getCountriesRequest(url);
+        return this.getCountriesRequest(url)
+        .pipe(
+            tap( countries => this.cacheStore.byRegion = { region, countries })
+        )
 
     }
     
